c634bf56eb833c0d19b5c4426dfb69b5b203fdb2
Dionykn <dionklein12@hotmail.com>
2024-02-09 04:08:17 +0100

    v0.0.2
    Clock and 8 hours of sleep done. Started home scene layout.

---
+++ Home.gd
@@ -0,0 +1,16 @@
+extends Control
+
+@onready var MainScene = get_node("/root/Node2D")
+
+func _ready():
+	pass
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
+
+
+func _on_sleep_pressed():
+	var i = 8
+	while i > 0:
+		i -= 1
+		MainScene.hours += 1
--- MainScene.gd
+++ MainScene.gd
@@ -1,51 +1,58 @@
 extends Node2D
 
 # Clock variables
-var hours = 6
-var minutes = 0
-var days = 1
+@export var hours = 6
+@export var minutes = 0
+@export var days = 1
+@export var formatted_time = "%02d:" % hours + "%02d" % minutes
+
 
 func _ready():
 	$Statusbar/Control/PanelContainer/MarginContainer/HBoxContainer/Date.text = "Day 1   06:00"
+	
 
 # Runs every 10s
 func _on_clock_timeout():
 	# Increment minutes directly
 	minutes += 1
-	print_debug(minutes)
-	# Handle time rollovers
-	if minutes >= 60:
-		minutes = 0
-		hours += 1
-	if hours >= 24:
-		hours = 0
-		days += 1
-	# Format and display the time
-	var formatted_time = "%02d:" % hours + "%02d" % minutes
-	$Statusbar/Control/PanelContainer/MarginContainer/HBoxContainer/Date.text = "Day " + str(days) +"   "+ str(formatted_time)
+
 
 # Runs every frame
 func _process(_delta):
+	# Disable navigation button to current scene
 	disable_navbutton()
+	# Handle time rollovers
+	if minutes >= 60:
+		minutes -= 60
+		hours += 1
+	if hours >= 24:
+		hours -= 24
+		days += 1
+	# Format and update the time
+	var previous_time = formatted_time
+	formatted_time = "%02d:" % hours + "%02d" % minutes
+	if formatted_time != previous_time:
+		$Statusbar/Control/PanelContainer/MarginContainer/HBoxContainer/Date.text = "Day " + str(days) +"   "+ str(formatted_time)
 
+# Set Home as visible scene
 func _on_home_pressed():
 	$Gameplay/Home_tscn.visible = true
 	$Gameplay/Survivor_tscn.visible = false
 	$Gameplay/Adventure_tscn.visible = false
 
-
+# Set Adventure as visible scene
 func _on_adventure_pressed():
 	$Gameplay/Home_tscn.visible = false
 	$Gameplay/Survivor_tscn.visible = false
 	$Gameplay/Adventure_tscn.visible = true
-	
-	
+
+# Set Survivor as visible scene	
 func _on_survivor_pressed():
 	$Gameplay/Home_tscn.visible = false
 	$Gameplay/Survivor_tscn.visible = true
 	$Gameplay/Adventure_tscn.visible = false
 
-
+# Disable navigation button to current scene
 func disable_navbutton():
 	if $Gameplay/Home_tscn.visible == true:
 		$Navigationbar/Control/PanelContainer/MarginContainer/HBoxContainer/Home.disabled = true
